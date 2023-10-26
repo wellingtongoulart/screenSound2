3f7d8c391a4397ee4981ccc63b3757c73c688d28
wellingtongoulart <wellingtongoulart@outlook.com>
2023-10-26 13:31:58 -0300

    feat: novas propriedades da classe música; feat: método para exibir a ficha técnica da musica.

--- screenSound2/Program.cs
+++ screenSound2/Program.cs
@@ -1,5 +1,28 @@
 ﻿Musica musica1 = new Musica();
-musica1.nome = string.Empty;
+musica1.Nome = "Roxane";
+musica1.Artista = "The Police";
+musica1.Duracao = 273;
+musica1.Disponivel = true;
+Console.WriteLine(musica1.DescricaoResumida);
+
+Musica musica2 = new Musica();
+musica2.Nome = "Vertigo";
+musica2.Artista = "U2";
+musica2.Duracao = 367;
+musica2.Disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
+List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+List<int> numerosPares = numeros.FindAll(numero => numero % 2 == 0);
+numerosPares.ForEach(numero => Console.WriteLine(numero));
+
+
+
+
+
+
 
 
 
